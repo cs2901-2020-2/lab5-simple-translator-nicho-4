9dd4adc17baf4f96bf728b4ca8c641af6fbebcfc
THEFLILUX <jorge.nicho@utec.edu.pe>
2020-09-23 09:42:22 -0500

    source

---
+++ src/main/java/cs/lab/DNASequencer.java
@@ -0,0 +1,13 @@
+package cs.lab;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+public class  DNASequencer {
+
+    static final Logger logger = Logger.getLogger(DNASequencer.class.getName());
+    public DNASequencer() {
+        logger.info("Starting sequencer...");
+    }
+
+}
